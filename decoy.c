5097bc2512f5b05145ad8f8475ab20b495ffe9f3
Andrey Yao <awy32@cornell.edu>
2022-03-15 17:37:32 -0400

    HEHE

---
+++ decoy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   int a = 9;
+   int b = 10;
+   int c = a * b;
+   printf(c);
+   return 0;
+}
